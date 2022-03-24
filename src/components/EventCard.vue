e91ec3d5e2acca04cb296237b34dd5218f891467
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-03-24 13:08:08 -0500

    Dynamic Routing

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="event-card">
-    <span>@{{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.title }}</h4>
-  </div>
+  <!-- v-bind to an object, inside the object, we specified the name of the route we want to route to. Route to the name of the component Make it more scalible, dont need to go to each route and update the path manually, easier to maintain. All you need to change of the route itself, to update -->
+  <!-- params === add event id to router's parameter.  -->
+  <router-link
+    class="event-link"
+    :to="{ name: 'EventDetails', params: { id: event.id } }"
+  >
+    <div class="event-card">
+      <span>@{{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>
+  |
 </template>
 
 <script>
@@ -26,5 +34,10 @@ export default {
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
 }
 </style>
